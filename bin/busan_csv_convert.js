7d1d22d86c200c96f6694921e5ea1e3c5afa4887
배성한 <furtur3@gmail.com>
2017-06-02 16:08:45 +0900

    develop, add generate node_geo.json, argument on start

--- bin/busan_csv_convert.js
+++ bin/busan_csv_convert.js
@@ -1,9 +1,28 @@
 var fs = require('fs');
 var parse = require('csv-parse');
 var leftPad = require('left-pad')
-var c_meta = require('./node_geo.json');
 
-var tb_node_raw = fs.readFileSync('source/busan_tb_node_raw_1215.csv', 'utf8');
+const args = process.argv;
+console.log(args);
+
+var srcFile;
+// check parameter
+if (args[2] == null) {
+  console.log('$>node busan_csv_convert.js csvfilename!!!!');
+  console.log('ex) $>node busan_csv_convert.js ../source/busan_tb_node_raw_1215.csv');
+  process.exit(1);
+}
+srcFile = args[2];
+
+// check if file exists
+if (fs.existsSync(srcFile)) {
+  console.log('Found file ' + srcFile);
+} else {
+  console.log('Not Found file ' + srcFile);
+  process.exit(1);
+}
+
+var tb_node_raw = fs.readFileSync(srcFile, 'utf8');
 parse(tb_node_raw, {comment:"#"}, function(csv_err, csv_data){
   if (csv_err) {
     return console.log(csv_err);
@@ -15,6 +34,27 @@ parse(tb_node_raw, {comment:"#"}, function(csv_err, csv_data){
   // console.log("%j",csv_data[1]);
   // console.log("%j",csv_data[2]);
 
+  // 1. Node에 대한 geo meta 정보를 node_geo.json 파일로 생성한다.
+  for(var cd=1; cd< csv_data.length ; cd+=1) {
+    var dataIn = csv_data[cd];
+    // Node의 Unigue GEO 정보를 node_geo.json에서 읽는다.
+    if (dataIn[22] !== "") {
+      geo_meta[dataIn[0]] = {GEO_LON : dataIn[21], GEO_LAT : dataIn[22]};
+    }
+
+  }
+  // 2. Node의 GEO 정보를 파일로 저장한다.
+  geo_meta = JSON.stringify(geo_meta,null,'\t');
+  fs.writeFileSync('./node_geo.json', geo_meta, function(err) {
+    if(err) return console.error(err);
+  })
+  console.log('Completed to generate node_geo.json File!!!');
+  // process.exit(1);
+
+  // 3. Node별 geo meta 정보 파일을 읽는다.
+  var c_meta = require('./node_geo.json');
+
+  // 4.corecode log 파일을 일자별로 생성한다.
   for(var cd=1; cd< csv_data.length ; cd+=1) {
   // for(var cd=1; cd< 1000 ; cd++){
     var dataIn = csv_data[cd];
@@ -28,12 +68,20 @@ parse(tb_node_raw, {comment:"#"}, function(csv_err, csv_data){
     var ldate = new Date(dataIn[3]);
     // console.log(dataIn[0] + ", " + dataIn[3]);
 
-    // Node의 Unigue GEO 정보를 읽는다.
-    if (dataIn[22] !== "") {
-      // geo_meta[dataIn[0]] = {GEO_LON : dataIn[21], GEO_LAT : dataIn[22]};
+    if (dataIn[22] === "") {
+      try {
+        dataIn[22] = c_meta[dataIn[0]].GEO_LAT;
+      } catch(e) {
+        console.log("Not found geo data => data[0] : %j, %j", dataIn[0], dataIn[1]);
+      }
     }
-    if (dataIn[22] === "") dataIn[22] = c_meta[dataIn[0]].GEO_LAT;
-    if (dataIn[21] === "") dataIn[21] = c_meta[dataIn[0]].GEO_LON;
+    if (dataIn[21] === "") {
+      try {
+        dataIn[21] = c_meta[dataIn[0]].GEO_LON;
+      } catch(e) {
+        ;
+      }
+    }
     console.log("%s, %s, %s, %s", dataIn[0], dataIn[3], dataIn[22], dataIn[21]);
 
     // console.log("node_id : " + node_id);
@@ -85,7 +133,7 @@ parse(tb_node_raw, {comment:"#"}, function(csv_err, csv_data){
     //console.log(ldate.toISOString().slice(0,10).replace(/-/g,""));
     var fileName = "corecode_"+ldateTemp[0]+leftPad(ldateTemp[1], 2, '0')+leftPad(ldateTemp[2], 2, '0')+".log";
     //var fileName = "1to4_"+ldate.toISOString().slice(0,10).replace(/-/g,"")+".log";
-    var path_file = "data/"+fileName;
+    var path_file = "../data/"+fileName;
 
     // for test, truncate file
     if (cd == 1 && fs.existsSync(path_file)) {
@@ -93,12 +141,6 @@ parse(tb_node_raw, {comment:"#"}, function(csv_err, csv_data){
     }
     var logdata = JSON.stringify(s_logs)+"\n";
     fs.appendFileSync(path_file, logdata);
-
   }
-
-  // Node의 GEO 정보를 파일로 저장한다.
-  // geo_meta = JSON.stringify(geo_meta,null,'\t');
-  // fs.writeFileSync('./bin/node_geo.json', geo_meta, function(err) {
-  //   if(err) return console.error(err);
-  // })
+  console.log('Completed to generate log file by day (totalcount : ' + csv_data.length + ')!!!');
 });
